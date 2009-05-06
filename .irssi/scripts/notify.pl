229e87489bc1f8b3254b59ade5f99d3c3071b86e
DecoDe <code723@gmail.com>
2009-05-07 00:00:08 +0800

    add notify.pl for irssi

--- .irssi/scripts/notify.pl
+++ .irssi/scripts/notify.pl
@@ -11,22 +11,62 @@ use vars qw($VERSION %IRSSI);
 
 $VERSION = "0.01";
 %IRSSI = (
-    authors     => 'Luke Macken',
-    contact     => 'lewk@csh.rit.edu',
+    authors     => 'Luke Macken, Paul W. Frields',
+    contact     => 'lewk@csh.rit.edu, stickster@gmail.com',
     name        => 'notify.pl',
-    description => 'TODO',
+    description => 'Use libnotify to alert user to hilighted messages',
     license     => 'GNU General Public License',
     url         => 'http://lewk.org/log/code/irssi-notify',
 );
 
+Irssi::settings_add_str('notify', 'notify_icon', 'gtk-dialog-info');
+Irssi::settings_add_str('notify', 'notify_time', '5000');
+
 sub notify {
+    my ($server, $summary, $message) = @_;
+
+    # Make the message entity-safe
+    $message =~ s/&/&amp;/g; # That could have been done better.
+    $message =~ s/</&lt;/g;
+    $message =~ s/>/&gt;/g;
+    $message =~ s/'/&apos;/g;
+
+    my $cmd = "EXEC - notify-send" .
+	" -i " . Irssi::settings_get_str('notify_icon') .
+	" -t " . Irssi::settings_get_str('notify_time') .
+	" -- '" . $summary . "'" .
+	" '" . $message . "'";
+
+    $server->command($cmd);
+}
+ 
+sub print_text_notify {
     my ($dest, $text, $stripped) = @_;
     my $server = $dest->{server};
 
     return if (!$server || !($dest->{level} & MSGLEVEL_HILIGHT));
-
-    $stripped =~ s/[^a-zA-Z0-9 .,!\?@\:\<\>]//g;
-    system("notify-send -i gtk-dialog-info -t 5000 '$dest->{target}' '$stripped'");
+    my $sender = $stripped;
+    $sender =~ s/^\<.([^\>]+)\>.+/\1/ ;
+    $stripped =~ s/^\<.[^\>]+\>.// ;
+    my $summary = $dest->{target} . ": " . $sender;
+    notify($server, $summary, $stripped);
 }
 
-Irssi::signal_add('print text', 'notify');
+sub message_private_notify {
+    my ($server, $msg, $nick, $address) = @_;
+
+    return if (!$server);
+    notify($server, "Private message from ".$nick, $msg);
+}
+
+sub dcc_request_notify {
+    my ($dcc, $sendaddr) = @_;
+    my $server = $dcc->{server};
+
+    return if (!$dcc);
+    notify($server, "DCC ".$dcc->{type}." request", $dcc->{nick});
+}
+
+Irssi::signal_add('print text', 'print_text_notify');
+Irssi::signal_add('message private', 'message_private_notify');
+Irssi::signal_add('dcc request', 'dcc_request_notify');
