982c5946f67f27b87e9119646cad261e4594282b
DecoDe <code723@gmail.com>
2011-02-25 21:26:27 +0800

    add list window shell script using wmctrl

---
+++ tools/list_win.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#wmctrl -l | grep "0x[a-z0-9]*  0" | sed "s|.* $(hostname) \([a-zA-Z /0-9\-]*\)|\1|" \
+#  | dmenu -l 10 -i -b -fa "Lihei Pro-10" -nb "#0a0a0a" -nf "#a0a0a0" -sb "#285577" -sf "#ffffff" -i -b -p "Goto: " \
+#  | xargs wmctrl -a
+LINE=`wmctrl -l | wc -l`  
+#wmctrl -l | cut -d' ' -f5- | dmenu -l $LINE -nb '#3f3f3f' -nf '#dcdccc' -sf '#3f3f3f' -sb '#dcdccc' -i | xargs wmctrl -a
+wmctrl -l | cut -d' ' -f5- | dmenu -l $LINE -fa "Lihei Pro-13" -nb '#0a0a0a' -nf '#a0a0a0' -sf '#fff' -sb '#285577' -i -p "Goto> " | xargs wmctrl -a
